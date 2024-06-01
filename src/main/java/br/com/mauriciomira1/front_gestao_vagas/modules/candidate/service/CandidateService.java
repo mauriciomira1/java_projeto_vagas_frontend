495d5bb91a53b0940cc725ad38783f7e100d9d8c
Maurício Miranda <mauriciomirandagomes@gmail.com>
2024-06-01 17:17:35 -0300

    continue

--- src/main/java/br/com/mauriciomira1/front_gestao_vagas/modules/candidate/service/CandidateService.java
+++ src/main/java/br/com/mauriciomira1/front_gestao_vagas/modules/candidate/service/CandidateService.java
@@ -6,8 +6,10 @@ import java.util.Map;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
+import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
+@Service
 public class CandidateService {
 
   public void login(String username, String password) {
